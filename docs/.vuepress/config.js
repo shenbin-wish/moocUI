94d7dc4f8b1e1c30f47d67b2db133b74227dc190
shenbin <823227928@qq.com>
2021-12-12 14:02:12 +0800

    ff

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,14 @@
 module.exports = {
+    title:'Mooc-UI',
+    base:'moocUI/',
     themeConfig: {
+      nav:[{
+        text: 'Home', link: '/'
+      },{
+        text: 'Github', link: 'https://github.com/shenbin-wish/moocUI'
+      },{
+        text: 'Npm', link: ''
+      }],
       sidebar: [
         '/',
         '/componentDocs/card'
